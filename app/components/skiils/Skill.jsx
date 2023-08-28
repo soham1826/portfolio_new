d6c3440e7b76c16f2085078891209f4c27dfd587
soham Kulkarni <soham18262@gmail.com>
2023-08-28 14:49:42 +0530

    bug fix again

--- app/components/skiils/Skill.jsx
+++ app/components/skiils/Skill.jsx
@@ -28,8 +28,10 @@ const Skill = ({imageSrc , bgColor,completed}) => {
       <Image 
       src="/assets/react.svg" className="rounded-md w-16 h-16 border border-Scream object-contain xl:h-32 xl:w-32 md:h-24 md:w-24 filter group-hover:grayscale transition duration-300 ease-in-out" width={300} height={300} alt="image"/>
 
-      <div className="absolute opacity-0 group-hover:opacity-90 transition duration-100 ease-in-out group-hover:bg-Scream w-16 h-16 xl:h-32 xl:w-32 md:h-24 md:w-24 rounded-md ">
-        <div className="flex items-center justify-center h-full p-2 " onMouseEnter={()=>{setPercent(completed)}} onMouseLeave={()=>{setPercent(-10)}}>
+      <div className="absolute opacity-0 group-hover:opacity-90 transition duration-100 ease-in-out group-hover:bg-Scream w-16 h-16 xl:h-32 xl:w-32 md:h-24 md:w-24 rounded-md " onMouseEnter={()=>{setTimeout(()=>{setPercent(completed)},500);}}
+        onMouseLeave={()=>{setPercent(0)}}>
+        <div className="flex items-center justify-center h-full p-2 " 
+        >
           <p className="absolute xl:text-xl text-sm font-poppins">{completed}%</p>
           <Circle percent={percent} strokeWidth={8} gapPosition="top" strokeColor="green" trailColor="gray" trailWidth={7}/>
         </div>
