9febd24f3aac4f7405ba34d024bfa604fb13023f
soham Kulkarni <soham18262@gmail.com>
2023-08-28 14:35:52 +0530

    Bug fix

--- app/components/skiils/Skills.jsx
+++ app/components/skiils/Skills.jsx
@@ -9,7 +9,7 @@ const Skills = () => {
     initial={{opacity:0}}
     whileInView={{opacity:1}}
     transition={{duration:1.2}}
-    className="h-screen flex relative flex-col md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-0 items-center text-2xl">
+    className="h-screen flex relative flex-col xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-0 items-center text-2xl">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-Scream font-poppins">Skills</h3>
       <p className="absolute top-36 uppercase font-poppins text-lg text-gray-300 opacity-20 tracking-wider"> Hover/Click Over to see proficiancy</p>
 
