58943fa3e544ecc9bdcf5f2e223f01446569e934
soham Kulkarni <soham18262@gmail.com>
2023-09-14 13:19:29 +0530

    again some changes in navbar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -18,37 +18,37 @@ const Nav = () => {
     <nav className="sticky top-0 font-poppins text-xl p-4 z-20 ">
       <div className="w-full justify-between items-center flex flex-row h-auto bg-transparent backdrop-blur-sm">
         {/* logo */}
-        <div className="flex space-x-2 items-center cursor-pointer">
+        <a href="#Hero" className="flex space-x-2 items-center cursor-pointer">
           <CodeBracketSquareIcon className="w-10 h-10 text-Scream animate-pulse"/>
           <p className="text-Swhite">Soham.dev</p>
-        </div>
+        </a>
         {/* navigation */}
         <div className=" flex-row items-center justify-center lg:flex hidden gap-2 mr-10">
-          <a href="#Hero " className="nav-link2 flex flex-row gap-1 items-center group w-[100px]">
+          <a href="#Hero " className="nav-link2 flex flex-row gap-1 items-center justify-center group w-[100px]">
           <HomeIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
           <p>Home</p>
           </a>
-          <a href="#About " className="nav-link2 flex flex-row gap-1 items-center group w-[100px]">
+          <a href="#About " className="nav-link2 flex flex-row gap-1 items-center group w-[100px] justify-center">
             <UserCircleIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
             About
 
             </a>
-          <a href="#Skills " className="nav-link2 flex flex-row gap-1 items-center group w-[100px]">
+          <a href="#Skills " className="nav-link2 flex flex-row gap-1 items-center group w-[100px] justify-center">
             <TrophyIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
             Skills
 
             </a>
-          <a href="#Experiance " className="nav-link2 flex flex-row gap-1 items-center group w-[150px]">
+          <a href="#Experiance " className="nav-link2 flex flex-row gap-1 items-center group w-[150px] justify-center">
             <BriefcaseIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
             Experiance
 
             </a>
-          <a href="#Projects " className="nav-link2 flex flex-row gap-1 items-center group w-[120px]">
+          <a href="#Projects " className="nav-link2 flex flex-row gap-1 items-center group w-[120px] justify-center">
             <PuzzlePieceIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
             Projects
 
             </a>
-          <a href="#Contact " className="nav-link2 flex flex-row items-center group w-[120px] gap-1">
+          <a href="#Contact " className="nav-link2 flex flex-row items-center group w-[120px] gap-1 justify-center">
             <EnvelopeIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
             Contact
 
@@ -66,49 +66,66 @@ const Nav = () => {
 
 
       </div>
+      {/* Mobile Navigation */}
+
       {ToggleMobile && (
           <motion.div
           initial={{
-            x:50,
+            x:100,
+            z:20,
             opacity:0
           }}
-          whileInView={{
+          animate={{
             x:0,
+            z:20,
             opacity:1
           }}
           transition={{
-            duration:1,
-            type:easeInOut
+            
           }}
-          className=" flex-col items-center flex lg:hidden gap-1  w-[60%] bg-Sblack h-screen justify-evenly absolute right-0 top-0 rounded-l-lg z-20 transition-all duration-100">
-           <div className="flex items-end">
-            <button onClick={handleClose}><XCircleIcon className="w-8 h-8 text-Scream focus:animate-spin duration-75 "/></button>
+          
+          className=" flex-col items-center flex lg:hidden w-[60%] bg-Sblack h-screen justify-evenly absolute right-0 top-0 rounded-l-lg z-20 transition-all duration-100 border-l border-gray-400">
+           <div className="flex justify-end w-full pr-2 mt-1">
+            <motion.button
+            initial={{
+              opacity:0,
+              scale:0
+            }}
+            whileInView={{
+              opacity:1,
+              scale:1
+            }}
+            transition={{
+              duration:0.5
+            }}
+             onClick={handleClose}><XCircleIcon className="w-10 h-10 text-Scream focus:animate-spin duration-75  "/></motion.button>
            </div>
-             <a href="#Hero " className="nav-link2 flex flex-row gap-1 items-center justify-center group w-full">
+           <div className="flex flex-col gap-4 items-center justify-center py-10 h-full">
+           <a href="#Hero " className="nav-link2 p-7 flex flex-row gap-1 items-center justify-center group w-full" onClick={handleClose}>
              <HomeIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
              <p>Home</p>
              </a>
-             <a href="#About " className="nav-link2 flex flex-row gap-1 items-center group w-full justify-center">
+             <a href="#About " className="nav-link2 p-7 flex flex-row gap-1 items-center group w-full justify-center" onClick={handleClose}>
                <UserCircleIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
                About
    
                </a>
-             <a href="#Skills " className="nav-link2 flex flex-row gap-1 items-center group w-full justify-center">
+             <a href="#Skills " className="nav-link2 p-7 flex flex-row gap-1 items-center group w-full justify-center" onClick={handleClose}>
                <TrophyIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
                Skills
    
                </a>
-             <a href="#Experiance " className="nav-link2 flex flex-row gap-1 items-center group w-full justify-center">
+             <a href="#Experiance " className="nav-link2 p-7 flex flex-row gap-1 items-center group w-full justify-center" onClick={handleClose}>
                <BriefcaseIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
                Experiance
    
                </a>
-             <a href="#Projects " className="nav-link2 flex flex-row gap-1 items-center group w-full justify-center">
+             <a href="#Projects " className="nav-link2 p-7 flex flex-row gap-1 items-center group w-full justify-center" onClick={handleClose}>
                <PuzzlePieceIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
                Projects
    
                </a>
-             <a href="#Contact " className="nav-link2 flex flex-row items-center group w-full justify-center gap-1">
+             <a href="#Contact " className="nav-link2 p-7 flex flex-row items-center group w-full justify-center gap-1" onClick={handleClose}>
                <EnvelopeIcon className="w-6 h-6 group-hover:animate-bounce text-Scream group-hover:text-black"/>
                Contact
              </a>
@@ -116,6 +133,8 @@ const Nav = () => {
              <div className="resume-button">
                Resume
              </div>
+           </div>
+            
            </motion.div>
          
       )}
@@ -123,7 +142,7 @@ const Nav = () => {
       
 
 
-    <div className={`absolute w-screen h-screen bg-transparent ${ToggleMobile ? "backdrop-blur-sm":""}`}>
+    <div className={`absolute w-screen h-screen bg-transparent lg:hidden  ${ToggleMobile ? "backdrop-blur-sm":""} ${ToggleMobile?"flex":"hidden"} `}>
 
     </div>
     </nav>
--- app/page.js
+++ app/page.js
@@ -8,36 +8,42 @@ import Contact from "./components/contact/Contact"
 import Link from "next/link"
 import Nav from "./components/Nav"
 
+// snap-y snap-mandatory 
+// snap-center
+// snap-start
+// snap-start
+
+
 const page = () => {
   return (
-    <div className="font-bold h-screen w-full bg-[#1A1C20] overflow-x-hidden overflow-y-auto snap-y snap-mandatory main scrollbar-none scrollbar-track-gray-400/20 scrollbar-thumb-Scream">
+    <div className=" scroll-smooth font-bold h-screen w-full bg-[#1A1C20] overflow-x-hidden overflow-y-auto main scrollbar-none scrollbar-track-gray-400/20 scrollbar-thumb-Scream">
       <Nav/>
-      <section id="Hero" className=" snap-center" >
+      <section id="Hero" className=" " >
           <Hero/>
       </section>
-      <section id="About" className=" snap-start xl:overflow-y-hidden overflow-y-auto  scrollbar-track-gray-400/20 scrollbar-thumb-Scream scrollbar-thin ">
+      <section id="About" className="  xl:overflow-y-hidden overflow-y-auto  scrollbar-track-gray-400/20 scrollbar-thumb-Scream scrollbar-thin ">
         <About/>
       </section>
-      <section id="Skills" className=" snap-start overflow-y-auto  scrollbar-track-gray-400/20 scrollbar-thumb-Scream scrollbar-thin">
+      <section id="Skills" className="  overflow-y-auto  scrollbar-track-gray-400/20 scrollbar-thumb-Scream scrollbar-thin">
         <Skills/>
       </section>
-      <section id="Experiance" className=" snap-center overflow-y-auto  scrollbar-track-gray-400/20 scrollbar-thumb-Scream scrollbar-thin">
+      <section id="Experiance" className="  overflow-y-auto  scrollbar-track-gray-400/20 scrollbar-thumb-Scream scrollbar-thin">
         <Experiance/>
       </section>
-      <section id="Projects" className="snap-center">
+      <section id="Projects" className="">
         <Projects/>
       </section>
-      <section id="Contact" className=" snap-start overflow-y-auto  scrollbar-track-gray-400/20 scrollbar-thumb-Scream scrollbar-thin">
+      <section id="Contact" className=" overflow-y-auto  scrollbar-track-gray-400/20 scrollbar-thumb-Scream scrollbar-thin">
         <Contact/>
       </section>
 
-          <footer className="sticky xl:bottom-5 bottom-8 w-full cursor-pointer">
+          {/* <footer className="sticky xl:bottom-5 bottom-8 w-full cursor-pointer">
             <div className="flex justify-center items-center px-5">
             <Link href="/#Hero">
               <img className="h-10 w-10  opacity-50 rounded-full grayscale hover:grayscale-0" src="assets/hero.png"/>
             </Link>
             </div>
-          </footer>
+          </footer> */}
 
 
 
